c321c1356757ba80ddfc2c2d3c565203aa537d6d
AnhTuan <113578763+TuanLBT@users.noreply.github.com>
2026-09-09 20:14:44 +0900

    Add hardware-agnostic source layer

---
+++ sources/__init__.py
@@ -0,0 +1,13 @@
+from sources.audio import AudioSource, MicVADAudioSource
+from sources.camera import CameraSource, OpenCVCameraSource
+from sources.chat import ChatSource
+from sources.screen import ScreenSource
+
+__all__ = [
+    "AudioSource",
+    "MicVADAudioSource",
+    "CameraSource",
+    "OpenCVCameraSource",
+    "ChatSource",
+    "ScreenSource",
+]
